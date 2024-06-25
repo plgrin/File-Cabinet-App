34f19d711ad2cb175e842d4a776660ec35f6e923
Polina <polina.green.minsk@gmail.com>
2024-06-25 20:48:36 +0300

    Add command line support for output-type, output, records-amount, and start-id options

--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace FileCabinetGenerator
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var outputTypeOption = new Option<string>(
+                new[] { "--output-type", "-t" },
+                getDefaultValue: () => "csv",
+                description: "Output format type (csv, xml)");
+
+            var outputOption = new Option<string>(
+                new[] { "--output", "-o" },
+                description: "Output file name.");
+
+            var recordsAmountOption = new Option<int>(
+                new[] { "--records-amount", "-a" },
+                getDefaultValue: () => 100,
+                description: "Amount of generated records.");
+
+            var startIdOption = new Option<int>(
+                new[] { "--start-id", "-i" },
+                getDefaultValue: () => 1,
+                description: "ID value to start.");
+
+            var rootCommand = new RootCommand
+            {
+                outputTypeOption,
+                outputOption,
+                recordsAmountOption,
+                startIdOption
+            };
+
+            rootCommand.Description = "FileCabinetGenerator - generates records for File Cabinet";
+
+            rootCommand.SetHandler((string outputType, string output, int recordsAmount, int startId) =>
+            {
+                Console.WriteLine($"Output Type: {outputType}");
+                Console.WriteLine($"Output: {output}");
+                Console.WriteLine($"Records Amount: {recordsAmount}");
+                Console.WriteLine($"Start ID: {startId}");
+
+            },
+            outputTypeOption, outputOption, recordsAmountOption, startIdOption);
+
+            rootCommand.InvokeAsync(args).Wait();
+        }
+    }
+}
