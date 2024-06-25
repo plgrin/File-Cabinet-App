a8430854cf36aa47fee722ee6cc144313a0478d4
Polina <polina.green.minsk@gmail.com>
2024-06-25 18:26:08 +0300

    Implement CreateRecord for FileCabinetFilesystemService.

--- FileCabinetApp/FileCabinetFilesystemService.cs
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -7,20 +7,67 @@ using System.Threading.Tasks;
 
 namespace FileCabinetApp
 {
+    /// <summary>
+    /// Provides file system based storage for File Cabinet records.
+    /// </summary>
     public class FileCabinetFilesystemService : IFileCabinetService
     {
+        private const int RecordSize = 278;
         private readonly FileStream fileStream;
         private readonly IRecordValidator validator;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetFilesystemService"/> class.
+        /// </summary>
+        /// <param name="fileStream">The file stream to use for storing records.</param>
+        /// <param name="validator">The validator to validate record parameters.</param>
         public FileCabinetFilesystemService(FileStream fileStream, IRecordValidator validator)
         {
             this.fileStream = fileStream;
             this.validator = validator;
         }
 
+        /// <summary>
+        /// Creates a new record in the file system.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="dateOfBirth">The date of birth.</param>
+        /// <param name="age">The age.</param>
+        /// <param name="salary">The salary.</param>
+        /// <param name="gender">The gender.</param>
+        /// <returns>The ID of the created record.</returns>
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char gender)
         {
-            throw new NotImplementedException();
+            var record = new FileCabinetRecord
+            {
+                Id = (int)(this.fileStream.Length / RecordSize) + 1,
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth,
+                Age = age,
+                Salary = salary,
+                Gender = gender,
+            };
+
+            this.validator.ValidateParameters(firstName, lastName, dateOfBirth, age, salary, gender);
+
+            using (BinaryWriter writer = new BinaryWriter(this.fileStream, System.Text.Encoding.UTF8, true))
+            {
+                writer.Seek(0, SeekOrigin.End);
+                writer.Write((short)0); // Status
+                writer.Write(record.Id);
+                writer.Write(PadRight(record.FirstName, 60));
+                writer.Write(PadRight(record.LastName, 60));
+                writer.Write(record.DateOfBirth.Year);
+                writer.Write(record.DateOfBirth.Month);
+                writer.Write(record.DateOfBirth.Day);
+                writer.Write(record.Age);
+                writer.Write(record.Salary);
+                writer.Write(record.Gender);
+            }
+
+            return record.Id;
         }
 
         public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char gender)
@@ -57,7 +104,10 @@ namespace FileCabinetApp
         {
             throw new NotImplementedException();
         }
+
+        private static char[] PadRight(string value, int length)
+        {
+            return value.PadRight(length).ToCharArray();
+        }
     }
-
-
 }
