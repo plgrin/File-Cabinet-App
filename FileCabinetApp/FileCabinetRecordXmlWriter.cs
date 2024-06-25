e94ea5a3cc44f1e0cce738046c0cd908bfee6264
Polina <polina.green.minsk@gmail.com>
2024-06-25 15:22:00 +0300

    Merge step6-add-export branch: Add export of file cabinet records to CSV and XML files.

---
+++ FileCabinetApp/FileCabinetRecordXmlWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetRecordXmlWriter
+    {
+        private readonly XmlWriter writer;
+
+        public FileCabinetRecordXmlWriter(XmlWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void Write(FileCabinetRecord record)
+        {
+            this.writer.WriteStartElement("record");
+            this.writer.WriteAttributeString("id", record.Id.ToString());
+
+            this.writer.WriteStartElement("name");
+            this.writer.WriteAttributeString("first", record.FirstName);
+            this.writer.WriteAttributeString("last", record.LastName);
+            this.writer.WriteEndElement();
+
+            this.writer.WriteElementString("dateOfBirth", record.DateOfBirth.ToString("MM/dd/yyyy"));
+
+            this.writer.WriteElementString("age", record.Age.ToString());
+            this.writer.WriteElementString("salary", record.Salary.ToString());
+            this.writer.WriteElementString("gender", record.Gender.ToString());
+
+            this.writer.WriteEndElement();
+        }
+    }
+}
---
+++ FileCabinetApp/FileCabinetServiceSnapshot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetServiceSnapshot
+    {
+        private readonly List<FileCabinetRecord> list;
+
+        public FileCabinetServiceSnapshot(List<FileCabinetRecord> list)
+        {
+            this.list = list;
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> Records => this.list.AsReadOnly();
+
+        public void SaveToCsv(StreamWriter writer)
+        {
+            var csvWriter = new FileCabinetRecordCsvWriter(writer);
+            csvWriter.WriteHeader();
+            foreach (var record in this.list)
+            {
+                csvWriter.Write(record);
+            }
+        }
+
+        public void SaveToXml(StreamWriter writer)
+        {
+            using var xmlWriter = XmlWriter.Create(writer, new XmlWriterSettings { Indent = true });
+            var recordWriter = new FileCabinetRecordXmlWriter(xmlWriter);
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("records");
+
+            foreach (var record in this.list)
+            {
+                recordWriter.Write(record);
+            }
+
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+        }
+    }
+
+}
